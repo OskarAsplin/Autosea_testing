0a5f71fb605b491770b3faaa1889ad77210c39d1
Oskar Asplin <oskarasplin@gmail.com>
2018-02-05 16:03:17 +0100

    More compact code

--- clutter_testing.py
+++ clutter_testing.py
@@ -6,7 +6,7 @@ import numpy as np
 
 clutterMap = clutter_maps.GeometricClutterMap(N_min = -500, N_max = 500, E_min = -500, E_max = 500, base_density = 1e-6)
 
-pprint.pprint(clutterMap.area)
+# pprint.pprint(clutterMap.area)
 
 def generate_square_regions(nLength = 20):
 	N_min = clutterMap.N_min
@@ -18,16 +18,11 @@ def generate_square_regions(nLength = 20):
 		while vertices[0][1] < clutterMap.E_max:
 			region = clutter_maps.SquareRegion(density = clutterMap.base_density, vertices = vertices)
 			clutterMap.add_region(region)
-			# vertices[:][1] += nLength
-			for v in vertices:
-				v[1] += nLength
-		# vertices[:][0] += nLength
-		for k, v in enumerate(vertices):
-			v[0] += nLength
-			v[1] = E_min
-			if k > 1:
-				v[1] += nLength
+			vertices[:, 1] += nLength
+		vertices[:, 0] += nLength
+		vertices[0:1, 1] = E_min
+		vertices[2:3, 1] = E_min + nLength
 
 generate_square_regions()
 
-pprint.pprint(clutterMap.area)
+# pprint.pprint(clutterMap.area)
